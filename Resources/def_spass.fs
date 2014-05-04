5aaf09d13ea2a9742dc62bdfa594f927d2bf0dba
Guilherme G. Schardong <schardong@tartarus.inferno>
2014-05-04 19:13:49 -0300

    I think I fixed the specular lightning problems.

--- Resources/def_spass.fs
+++ Resources/def_spass.fs
@@ -11,7 +11,7 @@ uniform mat4 viewMatrix;
 vec4 g_ambientColor = vec4(0.1);
 
 in vec2 vTexCoord;
-in vec3 vLightPos[2];
+in vec3 vLightPos[4];
 
 void main()
 {
@@ -19,26 +19,23 @@ void main()
   if(length(normal_camera) == 0)
     fColor = vec4(0.8);
   else {
+    fColor = vec4(0.f);
     vec4 diff_color = texture(u_diffuseMap, vTexCoord);
     vec3 vertex_camera = (texture(u_vertexMap, vTexCoord)).xyz;
-    vec3 light_world = vLightPos[0];
-    vec3 light_dir = normalize((viewMatrix * vec4(light_world, 1)) - vec4(vertex_camera, 1)).xyz;
-
-    float diff = max(dot(normal_camera, light_dir), 0.f);
-    fColor = diff * diff_color + g_ambientColor / 2;
 	
-	light_world = vLightPos[1];
-	light_dir = normalize((viewMatrix * vec4(light_world, 1)) - vec4(vertex_camera, 1)).xyz;
-    diff = max(dot(normal_camera, light_dir), 0.f);
-	fColor += diff * diff_color + g_ambientColor / 2;
-    
-	vec3 reflection = normalize(reflect(light_dir, normal_camera));
-    float spec = max(dot(normalize(vertex_camera), reflection), 0.f);
-	   
-	if(diff != 0) {
-      spec = pow(spec, 64.f);
-      fColor.rgb += vec3(spec);
+    for(int i = 0; i < 4; i++) {
+      vec3 light_camera = (viewMatrix * vec4(vLightPos[i], 1)).xyz;
+      vec3 light_dir = normalize(light_camera - vertex_camera);
+      float diff = max(dot(normal_camera, light_dir), 0.f);
+      fColor += diff * diff_color + g_ambientColor / 4;
+      
+      vec3 R = reflect(-light_dir, normal_camera);
+      float spec = max(dot(normalize(-vertex_camera), R), 0.f);
+      
+      //if(diff != 0) {
+      //  fColor.rgb += vec3(pow(spec, 1.f));
+      //}
+      fColor += vec4(pow(spec, 2.f));
     }
   }
-
 }
--- Resources/def_spass.vs
+++ Resources/def_spass.vs
@@ -7,13 +7,15 @@ uniform mat4 modelMatrix;
 uniform mat4 projMatrix;
 
 out vec2 vTexCoord;
-out vec3 vLightPos[2];
+out vec3 vLightPos[4];
 
 void main()
 {
   mat4 MP = projMatrix * modelMatrix;
   vTexCoord = in_vTexCoord;
   vLightPos[0] = vec3(0, 6, 0);
-  vLightPos[1] = vec3(30, 10, 30);
+  vLightPos[1] = vec3(20, 6, 0);
+  vLightPos[2] = vec3(20, 6, 20);
+  vLightPos[3] = vec3(0, 6, 20);
   gl_Position = MP * vec4(in_vPosition, 1.0);
 }
