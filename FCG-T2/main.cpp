2f208b08a361306d0d0913f01237a5deaa4a8b86
Guilherme G. Schardong <schardong@tartarus.inferno>
2014-05-11 15:05:14 -0300

    Added the FCG-T2 project and sources.

---
+++ FCG-T2/main.cpp
@@ -0,0 +1,185 @@
+#include "tglconfig.h"
+#include "fcgt2config.h"
+#include "tinygl.h"
+#include "logger.h"
+#include "shader.h"
+#include "mesh.h"
+#include "grid.h"
+#include "sphere.h"
+#include "axis.h"
+
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#define GLM_FORCE_RADIANS
+
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/gtc/matrix_inverse.hpp>
+
+using namespace std;
+
+void init();
+void initGLUT(int argc, char** argv);
+void initGLEW();
+void destroy();
+void update();
+void draw();
+void reshape(int w, int h);
+void keyPress(unsigned char c, int x, int y);
+void specialKeyPress(int c, int x, int y);
+void exit_cb();
+
+int g_window = -1;
+
+glm::mat4 viewMatrix;
+glm::mat4 projMatrix;
+glm::vec3 g_eye;
+glm::vec3 g_center;
+
+bool initCalled = false;
+bool initGLEWCalled = false;
+
+
+int main(int argc, char** argv)
+{
+  Logger::getInstance()->setLogStream(&cout);
+  Logger::getInstance()->log(TINYGL_LIBNAME + string(" v") + to_string(TINYGL_MAJOR_VERSION) + "." + to_string(TINYGL_MINOR_VERSION));
+
+  initGLUT(argc, argv);
+  initGLEW();
+  init();
+
+  glutMainLoop();
+  return 0;
+}
+
+void initGLUT(int argc, char** argv)
+{
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH | GLUT_RGB);
+  glutInitWindowSize(800, 600);
+
+  g_window = glutCreateWindow("FCG-T1");
+  glutReshapeFunc(reshape);
+  glutDisplayFunc(update);
+  glutKeyboardFunc(keyPress);
+  glutSpecialFunc(specialKeyPress);
+
+  glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS);
+  glutCloseFunc(exit_cb);
+  glutSetKeyRepeat(GLUT_KEY_REPEAT_ON);
+}
+
+void initGLEW()
+{
+  GLenum err = glewInit();
+
+  if (err != GLEW_OK) {
+    Logger::getInstance()->error("Failed to initialize GLEW");
+    std::cerr << glewGetErrorString(err) << std::endl;
+    exit(1);
+  }
+
+  glClearColor(0.8f, 0.8f, 0.8f, 1.f);
+  glEnable(GL_DEPTH_TEST);
+  glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+  //glPointSize(2);
+
+  initGLEWCalled = true;
+}
+
+void init()
+{
+  g_eye = glm::vec3(3.0, 3.0, 3.0);
+  g_center = glm::vec3(0, 0, 0);
+  viewMatrix = glm::lookAt(g_eye, g_center, glm::vec3(0, 1, 0));
+  projMatrix = glm::perspective(static_cast<float>(M_PI / 4.f), 1.f, 0.1f, 1000.f);
+
+  /*Shader* g_shader = new Shader("../Resources/fcgt1.vs", "../Resources/fcgt1.fs");
+  g_shader->bind();
+  g_shader->bindFragDataLoc("out_vColor", 0);
+  g_shader->setUniformMatrix("viewMatrix", viewMatrix);
+  g_shader->setUniformMatrix("projMatrix", projMatrix);
+  TinyGL::getInstance()->addResource(SHADER, "fcgt1", g_shader);*/
+
+  initCalled = true;
+}
+
+void destroy()
+{
+  TinyGL::getInstance()->freeResources();
+}
+
+void update()
+{
+  if (!initCalled || !initGLEWCalled)
+    return;
+
+  draw();
+}
+
+void draw()
+{
+  if (!initCalled || !initGLEWCalled)
+    return;
+
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  TinyGL* glPtr = TinyGL::getInstance();
+  /*Shader* s = glPtr->getShader("fcgt1");
+
+  s->bind();*/
+
+  //Draw something here.
+  
+  
+  /*glBindVertexArray(0);
+  Shader::unbind();*/
+
+  glutSwapBuffers();
+  glutPostRedisplay();
+}
+
+void reshape(int w, int h)
+{
+  if (!initCalled || !initGLEWCalled)
+    return;
+
+  glViewport(0, 0, w, h);
+  projMatrix = glm::perspective(static_cast<float>(M_PI / 4.f), static_cast<float>(w) / static_cast<float>(h), 0.1f, 1000.f);
+
+  /*Shader* s = TinyGL::getInstance()->getShader("fcgt1");
+  s->bind();
+  s->setUniformMatrix("projMatrix", projMatrix);*/
+
+  Shader::unbind();
+}
+
+void keyPress(unsigned char c, int x, int y)
+{
+  switch (c) {
+  default:
+    printf("(%d, %d) = %d, %c", x, y, c, c);
+    break;
+  }
+}
+
+void specialKeyPress(int c, int x, int y)
+{
+  switch (c) {
+  default:
+    printf("(%d, %d) = %d", x, y, c);
+    break;
+  }
+}
+
+void exit_cb()
+{
+  glutDestroyWindow(g_window);
+  destroy();
+  exit(EXIT_SUCCESS);
+}
