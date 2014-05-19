0d29f4b0fe1e22788827b1803e0147524c03e65d
Guilherme Schardong <schardong@tecgraf.puc-rio.com>
2014-05-19 10:27:52 -0300

    Added some comments on the Harris corner detector function.

--- FCG-T2/harris.h
+++ FCG-T2/harris.h
@@ -8,6 +8,19 @@ extern "C" {
 #include "image.h"
 }
 
+/**
+ * HarrisCornerDetector.
+ * This method implements the Harris corner detector algorithm. The source
+ * image must be a grayscale image. The destination image is allocated in the
+ * function.
+ * References:
+ * http://www.cse.psu.edu/~rcollins/CSE486/lecture06.pdf
+ * http://www.mathworks.com/help/images/ref/corner.html
+
+ * @src_img: The original grayscale image.
+ * @dst_img: The final image where only the corners will be visible.
+ * @returns: A vector of corner coordinates in the image space.
+ */
 std::vector<glm::vec2> HarrisCornerDetector(Image* src_img, Image* dst_img);
 
 #endif // HARRIS_H
