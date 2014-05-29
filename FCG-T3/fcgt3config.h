dd4e5c65b1e3861dcf01d0db825a1083a880a6c9
Guilherme Schardong <schardon@tecgraf.puc-rio.br>
2014-05-29 13:53:09 -0300

    Made the same changes as the FCG-T2 main file.

--- FCG-T3/fcgt3config.h
+++ FCG-T3/fcgt3config.h
@@ -6,6 +6,9 @@
 const std::string WINDOW_TITLE = "FCG-T3";
 static int WINDOW_W = 800;
 static int WINDOW_H = 600;
-const int NUM_IMAGES = 9;
+const int NUM_PATTERNS = 9;
+const int NUM_FID = 4;
+const int NUM_SOCCER = 1;
+const int NUM_IMAGES = NUM_PATTERNS + NUM_FID + NUM_SOCCER;
 
 #endif
