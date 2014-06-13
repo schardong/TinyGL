2e4ac7a8b56e173b7d9f6c1175d87c8a16256cc2
Guilherme G. Schardong <gschardong@inf.puc-rio.br>
2014-06-13 02:55:37 -0300

    Modified the rules file of the INF2610-T4 module to get the image.* files from the HarrisCD module. Crappy dependency, but I think it is best thant replicating code all over the place.

--- INF2610-T4/rules.mk
+++ INF2610-T4/rules.mk
@@ -1,8 +1,8 @@
 MODULE := inf2610-t4
 
 fcg-t1_TARGET := t4_ssao
-fcg-t1_CXXFLAGS := -ITinyGL/src
+fcg-t1_CXXFLAGS := -ITinyGL/src -IHarrisCD/src
 fcg-t1_LIBS := -lglut -lGLEW -lGL
-fcg-t1_LOCALLIBS := $(tinygl_TARGET)
+fcg-t1_LOCALLIBS := $(tinygl_TARGET) $(harriscd_TARGET)
 
 include common-rules.mk
